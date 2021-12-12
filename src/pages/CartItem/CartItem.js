1124568a8bc840407f630241dbd87c0137ca4688
james <engrjayt200@gmail.com>
2021-12-12 13:08:49 +0100

    Add styling to shopping cart

---
+++ src/pages/CartItem/CartItem.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./cartItem.css";
+export default function CartItem({
+  item,
+  loading,
+  incrementItemAmount,
+  decrementItemAmount,
+  removeFromCart,
+  ...rest
+}) {
+  const { image_url, title, price, amount, total } = item; // De-structure cart-item details
+
+  return (
+    <>
+      <div className="CartContainer">
+        <div className="Cart-Items">
+          <div className="image-box">
+            <img src={image_url} style={{ height: "100px" }} />
+          </div>
+          <div className="about">
+            <h5 className="title">{title}</h5>
+            <h3 className="subtitle">James Oyanna</h3>
+            <button
+              onClick={() => removeFromCart(item)}
+              className="remove about"
+            >
+              Remove
+            </button>
+          </div>
+
+          <div className="counter">
+            {/* Increase number of chosen cart product */}
+            <div onClick={() => incrementItemAmount(item)} className="btn">
+              +
+            </div>
+            <div className="count">{amount}</div>
+            {/* Decrease number of chosen cart product */}
+            <div onClick={() => decrementItemAmount(item)} className="btn">
+              -
+            </div>
+          </div>
+          <div>{total}</div>
+          <div className="prices">
+            <div className="amount">${price}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -1,6 +1,6 @@
 import "./cartpage.css";
 //import { Link } from "react-router-dom";
-import CartItem from "../../components/CartItem";
+import CartItem from "../CartItem/CartItem";
 const CartPage = ({
   show,
   cart,
@@ -10,6 +10,7 @@ const CartPage = ({
 }) => {
   const sideDrrawerClass = ["sidedrawer"];
 
+
   if (show) {
     sideDrrawerClass.push("show");
   }
@@ -18,15 +19,28 @@ const CartPage = ({
     <>
       <div className={sideDrrawerClass.join(" ")}>
         <h5>YOUR CART</h5>
-        {cart?.map((item) => (
-          <CartItem
-            key={item.id}
-            item={item}
-            incrementItemAmount={incrementItemAmount}
-            decrementItemAmount={decrementItemAmount}
-            removeFromCart={removeFromCart}
-          />
-        ))}
+        <div>
+          {cart?.map((item) => (
+            <CartItem
+              key={item.id}
+              item={item}
+              incrementItemAmount={incrementItemAmount}
+              decrementItemAmount={decrementItemAmount}
+              removeFromCart={removeFromCart}
+            />
+          ))}
+          <div className="checkout">
+            <div className="total">
+              <div>
+                <div className="Subtotal">Sub-Total</div>
+                <div className="items">2 items</div>
+              </div>
+              <div className="total-amount">$6.18</div>
+            </div>
+              <button className="button">Checkout</button>
+       
+          </div>
+        </div>
       </div>
     </>
   );
