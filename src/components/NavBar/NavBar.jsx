67494c89eb3af2f1b301c004aa9b089f8a60d836
james <engrjayt200@gmail.com>
2022-01-06 10:33:42 +0100

    Modify Button on product page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ import logowhite from "../../assets/image/logo-white.png";
 
 
 import { useCartCounter } from "../../contexts/CartContext";
-//import SearchBox from './../Search/SearchBox';
+// import SearchBox from './../Search/SearchBox';
 
 const NavBar = () => {
   const {cartCounter} = useCartCounter();
@@ -60,7 +60,7 @@ const NavBar = () => {
                     id="search-input"
                     type="text"
                     className="search-input w-full h-10 text-sm text-gray-dark font-light p-4"
-                    placeholder="Search books, genres, authos,etc."
+                    placeholder="Search books, genres, authors,etc."
                   />
                   <span className="absolute w-10 h-10 flex inset-right-0 border-left bg-gray-lighter">
                     <svg
--- src/components/Search/SearchBox.js
+++ src/components/Search/SearchBox.js
@@ -6,47 +6,27 @@ import { useState } from "react";
 import { FILTERED_BOOKS } from "../../graphQL/queries";
 import SearchResult from './SearchResult';
 
-// const GET_BOOKS = gql`
-//   query book($id: ID!) {
-//     book(id: $id) {
-//       id
-  
-//     }
-//   }
-// `;
 
-//  const GET_BOOKS = gql`
-//    {
-//      books {
-//        title
-//      }
-//    }
-//  `;
-
-
-const SearchBox = ({limit}) => {
+const SearchBox = () => {
   const [filter, setFilter] = useState("");
 
-  const { data, loading, error, refetch } = useQuery(FILTERED_BOOKS, {
-    variables: { filter },
+  const { data, loading, error } = useQuery(FILTERED_BOOKS, {
+    limit: 10,
   });
 
-
-  //  const [executeSearch, { data, loading, error }] =
-  //    useLazyQuery(FILTERED_BOOKS);
 
 
      if (loading) return <div>Loading</div>;
      if (error) return <div>error</div>;
 
-       console.log("Result from Quidax app", data);
+      //  console.log("Result from Quidax app", data);
 
   return (
     <>
       <div className="App">
         <h1>
-          {/* <a href="#book" aria-hidden="true" className="aal_anchor book"> */}
-            {/* <svg
+          <a href="#book" aria-hidden="true" className="aal_anchor book">
+            <svg
               aria-hidden="true"
               class="aal_svg"
               height="16"
@@ -58,8 +38,8 @@ const SearchBox = ({limit}) => {
                 fill-rule="evenodd"
                 d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
               ></path>
-            </svg> */}
-          {/* </a> */}
+            </svg>
+          </a>
           {/* Books */}
         </h1>
 
@@ -84,7 +64,7 @@ const SearchBox = ({limit}) => {
         )}
         <br />
 
-        <button onClick={() => refetch()}>Submit!</button>
+        <button>Submit</button>
       </div>
     </>
   );
